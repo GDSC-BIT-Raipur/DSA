868142103a157019afb5b505c3b6dc14db4ed9d6
yogesh dubey <yogesh.dubey.0804@gmail.com>
2023-09-02 21:05:17 +0530

    removed queue

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,10 +19,9 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'GDSC-BIT-Raipur', // Usually your GitHub org/user name.
   projectName: 'DSA', // Usually your repo name.
-
   onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
-
+  trailingSlash: false,
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
